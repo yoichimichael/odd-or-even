2df1b85400599626862118f0c35018438e724559
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-12-17 00:21:43 -0500

    add  printing logic for odd or even values

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 
-	numArray := []int{0,1,2,3,4,5,6,7,8,9,10}
+	na := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	for _, val := range numArray {
-
+	for _, val := range na {
+		if val%2 == 0 {
+			fmt.Println(val, "is even")
+		} else {
+			fmt.Println(val, "is odd")
+		}
 	}
-
 }
